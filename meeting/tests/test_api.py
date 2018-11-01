bdb2f20ac819fe6a1765b070249524c6d896062b
Lucas Polo <pradopolo@gmail.com>
2018-11-01 13:35:39 -0300

    Criado método REST para inserir novas salas
    close #17

--- meeting/tests/test_api.py
+++ meeting/tests/test_api.py
@@ -1,4 +1,16 @@
 def test_get_salas(app):
     with app.test_client() as client:
-        result = client.get('/api/sala/')
-        assert result.status_code == 200
+        salas = client.get('/api/sala/')
+        assert salas.status_code == 200
+        assert 'salas' in salas.json
+
+
+def test_put_salas(app):
+    with app.test_client() as client:
+        nova_sala = {
+            'nome': 'Currie',
+            'capacidade': 10,
+        }
+
+        created = client.post('api/sala/', json=nova_sala)
+        assert created.status_code == 201
