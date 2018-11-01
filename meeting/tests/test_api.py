963046e21ba25baa88dab07cc73ee832bb1fabe5
Lucas Polo <pradopolo@gmail.com>
2018-11-01 14:51:15 -0300

    Criado método PUT para atualizar salas
    close #19

--- meeting/tests/test_api.py
+++ meeting/tests/test_api.py
@@ -1,11 +1,11 @@
-def test_get_salas(app):
+def test_recuperar_salas(app):
     with app.test_client() as client:
         salas = client.get('/api/sala/')
         assert salas.status_code == 200
         assert 'salas' in salas.json
 
 
-def test_put_salas(app):
+def test_inserir_sala(app):
     with app.test_client() as client:
         nova_sala = {
             'nome': 'Currie',
@@ -14,3 +14,18 @@ def test_put_salas(app):
 
         created = client.post('api/sala/', json=nova_sala)
         assert created.status_code == 201
+
+
+def test_atualizar_sala_existente(app):
+    registro = app.db['salas'].find_one({})
+    print("== ", registro['_id'], " ", registro['capacidade'])
+    with app.test_client() as client:
+        updated = client.put(f"api/sala/{registro['_id']}", json={
+            'nome': registro['nome'],
+            'capacidade': 40,
+        })
+
+        registro_atualizado = app.db['salas'].find_one({"_id": registro["_id"]})
+
+        assert updated.status_code == 200
+        assert registro_atualizado['capacidade'] == '40'
