7eff444bc7a4cbc5602be3be56be4494823384ab
Lucas Polo <pradopolo@gmail.com>
2018-11-01 13:35:39 -0300

    Criado método REST para inserir novas salas
    close #17

--- meeting/blueprints/restapi.py
+++ meeting/blueprints/restapi.py
@@ -1,14 +1,23 @@
 from flask import Blueprint
 from flask import current_app as app
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 bp = Blueprint('restapi', __name__, url_prefix='/api')
 api = Api(bp)
+
+sala_post_parser = reqparse.RequestParser()
+sala_post_parser.add_argument('nome', required=True)
+sala_post_parser.add_argument('capacidade', required=True)
 
 
 class Sala(Resource):
     def get(self):
         return {'salas': list(app.db['salas'].find())}
+
+    def post(self):
+        sala = sala_post_parser.parse_args()
+        nova_sala = app.db['salas'].insert({'nome': sala.nome, 'capacidade': sala.capacidade})
+        return {'sala criada': nova_sala.inserted_id}, 201
 
 
 def configure(app):
--- meeting/tests/conftest.py
+++ meeting/tests/conftest.py
@@ -5,4 +5,11 @@ from meeting.app import create_app
 
 @pytest.fixture(scope="module")
 def app():
-    return create_app()
+    app_obj = create_app()
+    app_obj.db['salas'].delete_many({})
+
+    app_obj.db['salas'].insert_one({'nome': 'Turing', 'capacidade': 20})
+
+    yield  app_obj
+
+    app_obj.db['salas'].delete_many({})
