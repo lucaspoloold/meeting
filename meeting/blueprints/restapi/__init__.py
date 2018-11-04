ef4d483900c5be48980ea34c7d078690fde4bc0c
Lucas Polo <pradopolo@gmail.com>
2018-11-04 14:36:22 -0200

    Criada estrutura e metodo GET para agendamentos
    close #27

--- meeting/blueprints/restapi/__init__.py
+++ meeting/blueprints/restapi/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
+from meeting.blueprints.restapi.agendamento_resource import AgendamentoList
 from meeting.blueprints.restapi.sala_resource import SalaList, Sala
 
 bp = Blueprint('restapi', __name__, url_prefix='/api')
@@ -10,4 +11,5 @@ api = Api(bp)
 def configure(app):
     api.add_resource(SalaList, '/sala/')
     api.add_resource(Sala, '/sala/<sala_id>')
+    api.add_resource(AgendamentoList, '/agendamento/')
     app.register_blueprint(bp)
--- meeting/ext/db.py
+++ meeting/ext/db.py
@@ -1,23 +1,12 @@
 from pymongo import MongoClient
-from tinydb_serialization import SerializationMiddleware
 from tinymongo import TinyMongoClient
-from tinymongo.serializers import DateTimeSerializer
-
-
-class CustomClient(TinyMongoClient):
-    @property
-    def _storage(self):
-        serialization = SerializationMiddleware()
-        serialization.register_serializer(DateTimeSerializer(), 'TinyDate')
-
-        return serialization
 
 
 def configure(app):
     if app.env == "production":
         client = MongoClient(host="localhost")
     else:
-        client = CustomClient('database')
+        client = TinyMongoClient('database')
 
     db_name = app.config.get('MONGODB_NAME', 'db')
     app.db = client[db_name]
