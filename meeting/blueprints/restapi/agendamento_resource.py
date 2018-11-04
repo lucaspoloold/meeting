8a98e267f89c04f1bb374c02568e2065b791d46a
Lucas Polo <pradopolo@gmail.com>
2018-11-04 19:40:51 -0200

    Refatorado método POST para isolar regra de agendamento
    close #33

--- meeting/blueprints/restapi/agendamento_resource.py
+++ meeting/blueprints/restapi/agendamento_resource.py
@@ -1,6 +1,8 @@
 from flask import current_app as app
 from flask_restful import Resource, reqparse
 
+from meeting.utils.agendamento_utils import valida_agendamento
+
 agendamento_post_parser = reqparse.RequestParser()
 agendamento_post_parser.add_argument('titulo', required=True)
 agendamento_post_parser.add_argument('sala_id', required=True)
@@ -10,30 +12,13 @@ agendamento_post_parser.add_argument('fim', required=True)
 
 class AgendamentoList(Resource):
 
-    @staticmethod
-    def __valida_agendamento(db, agendamento):
-        if not db['salas'].find_one({'_id': agendamento['sala_id']}):
-            raise Exception("A sala informada nao existe")
-
-        if app.db['agendamentos'].find_one({
-                'inicio': {'$lte': agendamento['inicio']},
-                'fim': {'$gte': agendamento['inicio']},
-                "sala_id": agendamento['sala_id']}):
-            raise Exception("Ja existe reuniao para esta sala neste horario")
-
-        if app.db['agendamentos'].find_one({
-                'inicio': {'$lte': agendamento['fim']},
-                'fim': {'$gte': agendamento['fim']},
-                "sala_id": agendamento['sala_id']}):
-            raise Exception("Ja existe reuniao para esta sala neste horario")
-
     def get(self):
         return {'agendamentos': list(app.db['agendamentos'].find())}
 
     def post(self):
         agendamento = agendamento_post_parser.parse_args()
         try:
-            self.__valida_agendamento(app.db, agendamento)
+            valida_agendamento(app.db, agendamento)
         except Exception as e:
             return {"Agendamento inválido": str(e)}, 409
         else:
