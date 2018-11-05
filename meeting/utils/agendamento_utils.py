f03cfced59ec528121ceea97eb1ef7424ab5c344
Lucas Polo <pradopolo@gmail.com>
2018-11-05 13:20:54 -0200

    Validando formato das datas do agendamento
    close #41

--- meeting/utils/agendamento_utils.py
+++ meeting/utils/agendamento_utils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def valida_sala(db, sala_id):
     if not db['salas'].find_one({'_id': sala_id, 'ativa': True}):
         raise Exception("A sala informada nao existe")
@@ -13,7 +16,19 @@ def valida_sala_livre(db, sala_id, horario, agendamento_id):
         raise Exception("Ja existe reuniao para esta sala neste horario")
 
 
+def valida_datas(agendamento):
+    dateformat = "%Y-%m-%d %H:%M:%S"
+    try:
+        datetime.strptime(agendamento['inicio'], dateformat)
+        datetime.strptime(agendamento['fim'], dateformat)
+    except Exception:
+        raise Exception("Formato de data/hora invalido (%Y-%m-%d %H:%M:%S), confira inicio e fim")
+
+
 def valida_agendamento(db, agendamento, agendamento_id=""):
+
+    valida_datas(agendamento)
+
     valida_sala(db, agendamento['sala_id'])
 
     valida_sala_livre(db, agendamento['sala_id'], agendamento['inicio'], agendamento_id)
