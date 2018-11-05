308512136312fa3846ffb8709ce42c94c0bccea2
Lucas Polo <pradopolo@gmail.com>
2018-11-05 13:03:46 -0200

    Criado método para alterar (PUT) o agendamento
    close #39

--- meeting/utils/agendamento_utils.py
+++ meeting/utils/agendamento_utils.py
@@ -3,16 +3,18 @@ def valida_sala(db, sala_id):
         raise Exception("A sala informada nao existe")
 
 
-def valida_sala_livre(db, sala_id, horario):
+def valida_sala_livre(db, sala_id, horario, agendamento_id):
     if db['agendamentos'].find_one({
             'inicio': {'$lte': horario},
             'fim': {'$gte': horario},
-            "sala_id": sala_id}):
+            "sala_id": sala_id,
+            "_id": {"$ne": agendamento_id},
+            }):
         raise Exception("Ja existe reuniao para esta sala neste horario")
 
 
-def valida_agendamento(db, agendamento):
+def valida_agendamento(db, agendamento, agendamento_id=""):
     valida_sala(db, agendamento['sala_id'])
 
-    valida_sala_livre(db, agendamento['sala_id'], agendamento['inicio'])
-    valida_sala_livre(db, agendamento['sala_id'], agendamento['fim'])
+    valida_sala_livre(db, agendamento['sala_id'], agendamento['inicio'], agendamento_id)
+    valida_sala_livre(db, agendamento['sala_id'], agendamento['fim'], agendamento_id)
